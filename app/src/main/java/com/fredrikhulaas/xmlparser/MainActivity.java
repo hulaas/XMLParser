bb45b481450d12618fe4b81c2b395db54372ac2c
unknown <fredrik-hulaas@hotmail.com>
2018-11-08 14:23:11 +0100

    first xmlpullparser

---
+++ app/src/main/java/com/fredrikhulaas/xmlparser/MainActivity.java
@@ -0,0 +1,97 @@
+package com.fredrikhulaas.xmlparser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    private TextView tv;
+    private TextView tv1;
+    private TextView tv2;
+    private TextView tv3;
+    private TextView tv4;
+    private TextView tv5;
+    private TextView tv6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        tv2 = (TextView) findViewById(R.id.tv2);
+        tv3 = (TextView) findViewById(R.id.tv3);
+        tv4 = (TextView) findViewById(R.id.tv4);
+        tv5 = (TextView) findViewById(R.id.tv5);
+        tv6 = (TextView) findViewById(R.id.tv6);
+        parseXml();
+    }
+
+    private void parseXml() {
+        XmlPullParserFactory parserFactory;
+        try {
+            parserFactory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = parserFactory.newPullParser();
+            InputStream is = getAssets().open("forecast.xml");
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(is, null);
+            processParsing(parser);
+
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processParsing(XmlPullParser parser) throws XmlPullParserException, IOException {
+        ArrayList<WeatherData> weatherList = new ArrayList<>();
+        int eventType = parser.getEventType();
+        String weather = "";
+        WeatherData currentWeather = null;
+
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            String tag = null;
+            switch (eventType) {
+                case XmlPullParser.START_TAG:
+                    tag = parser.getName();
+                    System.out.println(tag);
+                    if (parser.getName().equalsIgnoreCase("name") && parser.getAttributeCount() == 0) {
+                        tag = parser.nextText();
+                        tv.setText("Location:  " + tag);
+                    } else if (parser.getName().equalsIgnoreCase("credit")) {
+                        parser.nextTag();
+                        tag = parser.getAttributeValue(null, "text");
+                        tv5.setText("Fra yr:  " + tag);
+                    } else if (parser.getName().equalsIgnoreCase("temperature")){
+                        tag = parser.getAttributeValue(null, "value");
+                        weather = parser.getAttributeValue(null, "unit");
+                        tv1.setText("Temperature: " + tag + " " + weather);
+                    } else if (tag.equalsIgnoreCase("pressure")) {
+                        tag = parser.getAttributeValue(null,"value");
+                        tv2.setText("Pressure: " + tag);
+                    } else if (parser.getName().equalsIgnoreCase("title")) {
+                        tag = parser.nextText();
+                        tv3.setText("Today:  " + tag);
+                    } else if (parser.getName().equalsIgnoreCase("windDirection")) {
+                        tag = parser.getAttributeValue(null,"name");
+                        tv4.setText("WindDirection:  " + tag);
+                    } else if (parser.getName().equalsIgnoreCase("tabular")) {
+                        parser.nextTag();
+                        tag = parser.getAttributeValue(null,"from");
+                        weather = parser.getAttributeValue(null,"to");
+                        tv6.setText("Time from: " + tag + " " + "Time to: " + weather);
+                    }
+            }
+            eventType = parser.next();
+        }
+    }
+}
---
+++ app/src/main/java/com/fredrikhulaas/xmlparser/WeatherData.java
@@ -0,0 +1,95 @@
+package com.fredrikhulaas.xmlparser;
+
+/* data objekt som holder på informasjonen */
+public class WeatherData {
+    private String location;
+    private String title;
+    private String body;
+    private String time;
+    private String symbol;
+    private String precipitation;
+    private String windDirection;
+    private String windSpeed;
+    private String temperature;
+    private String pressure;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getPrecipitation() {
+        return precipitation;
+    }
+
+    public void setPrecipitation(String precipitation) {
+        this.precipitation = precipitation;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(String windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+}
